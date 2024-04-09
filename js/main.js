6c68b4c09c61039ef9559f30eba697c595e3c4fe
Juan <137791015+JuanDavidZabalaTapiero@users.noreply.github.com>
2024-04-09 14:32:52 -0500

    Commit 2
    Se añadió el modo noche al portafolio

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ function showNav(){
     let nav2 = document.querySelector(".nav-v2");
     nav2.classList.toggle("active");
 }
+function nightMode(){
+    let body = document.querySelector(".body");
+    body.classList.toggle("night");
+}
